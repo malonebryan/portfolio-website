1cd7e63f9b4a79d1897497216f74f16de5019960
malonebryan <bryanmalone5@gmail.com>
2023-05-23 13:13:51 -0400

    final touches

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,15 @@
 console.log("hello, welcome to my portfolio site")
+
 function menuToggle() {
     var x = document.getElementById('myNavtoggle');
     if (x.className === 'navtoggle') {
-      x.className += ' responsive';
+    x.className += ' responsive';
     } else {
-      x.className = 'navtoggle';
+    x.className = 'navtoggle';
     }
-  }
+}
+
+$('#goTop').on('click', function(e){
+    $("html, body").animate({scrollTop: $("#top").offset().top}, 500);
+    return false;
+    });
